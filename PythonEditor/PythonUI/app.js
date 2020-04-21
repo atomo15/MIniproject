0b8e7cd2764273b4291a8acd96e0ed707d375717
Thunnathorne <atom9583@gmail.com>
2020-04-21 02:55:46 -0500

    Update app.js

--- PythonEditor/PythonUI/app.js
+++ PythonEditor/PythonUI/app.js
@@ -3,9 +3,13 @@
 
 function updatedisplay() {
 	x = document.getElementById("stable-display-id").value;
+	if(x==null)
+	{
+		console.log("null")
+	}
 	y = document.getElementById("edit").value;
-	console.log(x+y)
-    document.getElementById("realtime-display").innerHTML = x+y;
+	//console.log(x+y)
+    document.getElementById("realtime-display").innerHTML = y;
 }
 
 </script>
