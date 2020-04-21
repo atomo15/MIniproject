71ce6b75bd1fa66f2dfb46974e294eddd84498cd
Thunnathorne <atom9583@gmail.com>
2020-04-21 03:54:04 -0500

    drop real time display

--- PythonEditor/PythonUI/app.js
+++ PythonEditor/PythonUI/app.js
@@ -1,16 +1,16 @@
 
-function updatedisplay() {
-	x = document.getElementById("stable-display-id").value;
-	y = document.getElementById("edit").value;
-	if(x==null)
-	{
-		document.getElementById("realtime-display").innerHTML = y;
-	}
-	else
-	{
-		document.getElementById("realtime-display").innerHTML = x+y;
-	}
-}
+// function updatedisplay() {
+// 	x = document.getElementById("stable-display-id").value;
+// 	y = document.getElementById("edit").value;
+// 	if(x==null)
+// 	{
+// 		document.getElementById("realtime-display").innerHTML = y;
+// 	}
+// 	else
+// 	{
+// 		document.getElementById("realtime-display").innerHTML = x+y;
+// 	}
+// }
 
 // function submitcode() {
 // 	x = document.getElementById("stable-display-id").value;
