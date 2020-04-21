c08e2925cba88e9382c012aae3adf0398b5c03fe
Thunnathorne <atom9583@gmail.com>
2020-04-21 04:14:21 -0500

    clear

--- PythonEditor/PythonUI/app.js
+++ PythonEditor/PythonUI/app.js
@@ -1,37 +1,4 @@
 
-// function updatedisplay() {
-// 	x = document.getElementById("stable-display-id").value;
-// 	y = document.getElementById("edit").value;
-// 	if(x==null)
-// 	{
-// 		document.getElementById("realtime-display").innerHTML = y;
-// 	}
-// 	else
-// 	{
-// 		document.getElementById("realtime-display").innerHTML = x+y;
-// 	}
-// }
-
-// function submitcode() {
-// 	x = document.getElementById("stable-display-id").value;
-// 	z = document.getElementById("realtime-display").value;
-
-// 	if(x==null)
-// 	{
-// 		document.getElementById("backupcode").value = "No backup code";
-// 		document.getElementById("stable-display-id").innerHTML = z;
-// 		document.getElementById("stable-display-id").value = z;
-// 	}
-// 	else
-// 	{
-// 		pz = x;
-// 		console.log(pz)
-// 		document.getElementById("backupcode").value = pz;
-// 		document.getElementById("backupcode").innerHTML = pz;
-// 		document.getElementById("stable-display-id").innerHTML = z;
-// 		document.getElementById("stable-display-id").value = z;
-// 	}
-// }
 
 function submitcode() {
 	x = document.getElementById("stable-display-id").value;
