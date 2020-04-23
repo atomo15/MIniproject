8141bdb31c22ff7566aaf6f0e1b766ee43c25514
Thunnathorne <atom9583@gmail.com>
2020-04-22 22:16:11 -0500

    Update convertor.js

--- PythonEditor/PythonUI/convertor.js
+++ PythonEditor/PythonUI/convertor.js
@@ -1,8 +1,18 @@
 function popup2pair() {
-	console.log("Hello")
 	var data1 = document.getElementById("data1");
+
+	if (data1.style.display === "block") {
+		console.log("Hello")
+    	data1.style.display = "none";
+  	} else {
+  		console.log("Hi")
+    	data1.style.display = "block";
+  	}
+	
 	var x = '<b><h1>KEY : </h1></b><input type="text">';
 	var y = '<b><h1>VALUES : </h1></b><input type="text">';
 	var z = '<br><br><a href="#popup2key" class="myButton" >Convert</a>'
 	data1.innerHTML= x+y+z;
+	
+  	
 }
